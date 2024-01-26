4078ad86d3dcd4c724be7fc0c521d97d5e4afcbd
jongheonleee <qwefghnm1212@gmail.com>
2024-01-26 17:01:36 +0900

    Doubly Linked List 2차 구현 및 테스트 완료

--- src/implemented/MyDoublyLinkedList.java
+++ src/implemented/MyDoublyLinkedList.java
@@ -1,5 +1,7 @@
 package implemented;
 
+import java.util.Collection;
+import java.util.Iterator;
 import test.MyDoublyLinkedListTest;
 
 public class MyDoublyLinkedList {
@@ -22,6 +24,17 @@ public class MyDoublyLinkedList {
         size = 0;
     }
 
+    public MyDoublyLinkedList(Collection c) {
+        if (c == null)
+            throw new NullPointerException();
+
+        Iterator it = c.iterator();
+        while (it.hasNext()) {
+            Object obj = it.next();
+            add(obj);
+        }
+    }
+
     // 리스트 뒤에 객체를 노드형태로 추가
     public boolean add(Object obj) {
         // 0. 객체 -> 노드
@@ -31,7 +44,6 @@ public class MyDoublyLinkedList {
             // 1-1-1. 첫번째 노드에 생성된 노드 기록
         if (size == 0) {
             first = newNode;
-            last = newNode;
         }
         // 1-2. 리스트가 비어있지 않은 경우
             // 1-2-1. 마지막 노드의 다음 링크에 연결
@@ -39,10 +51,10 @@ public class MyDoublyLinkedList {
         else {
             last.next = newNode;
             newNode.prev = last;
-            last = last.next;
         }
 
         // 2. 마지막 노드 생성된 노드로 기록 및 사이즈업
+        last = newNode;
         size++;
         return true;
     }
@@ -157,14 +169,236 @@ public class MyDoublyLinkedList {
         return removed;
     }
 
+    // index 번째 노드 탐색
     private Node find(int index) {
         Node curr = first;
-        for (int i=0; i<index; i++) {
-            curr = curr.next;
+        // 0. 앞에서 탐색할지 뒤에서 탐색할지 판단
+            // 0-1. 중간 위치보다 작은 곳에 있으면 앞에서 탐색
+            // 0-2. 중간 위치보다 같거나 큰 곳에 있으면 뒤에서 탐색
+        if (index < size/2) {
+            for (int i=0; i<index; i++) {
+                curr = curr.next;
+            }
+        } else {
+            curr = last;
+            for (int i=0; i<size-index-1; i++) {
+                curr = curr.prev;
+            }
         }
 
         return curr;
     }
+
+    // index 번째에 노드 추가하기
+    public void add(int index, Object obj) {
+        // 0. 검증
+            // 0-1. 인덱스가 허용 범위에 포함되는지
+        if (!isIncludeIndex(index))
+            throw new IndexOutOfBoundsException();
+
+        // 1. 노드 생성
+        Node newNode = new Node(obj);
+
+        // 2. 추가
+            // 2-0. 리스트가 비어있는 경우
+            // 2-1. 맨 앞 추가
+            // 2-2. 중간 추가
+            // 2-3. 맨 뒤 추가
+        if (size == 0) {
+            add(obj);
+        } else if (index == 0) {
+            newNode.next = first;
+            first.prev = newNode;
+            first = newNode;
+        } else if (0 < index && index < size) {
+            Node curr = find(index);
+            Node prev = curr.prev;
+
+            prev.next = newNode;
+            newNode.prev = prev;
+
+            newNode.next = curr;
+            curr.prev = newNode;
+        } else {
+            Node curr = last;
+
+            last.next = curr;
+            curr.prev = last;
+        }
+        // 3. 사이즈업
+        size++;
+    }
+
+    // index 번째에 데이터 더미를 추가함
+    public void addAll(int index, Collection c) {
+        // 0. 검증
+            // 0-1. 잘못된 인덱스 범위
+        if (!isIncludeIndex(index))
+            throw new IndexOutOfBoundsException();
+
+        // 1. index 번째 이동
+        Node curr = find(index);
+
+        // 2. 이터레이터 생성
+        Iterator it = c.iterator();
+        int tmpSize = 0;
+
+        // 3. 이어 붙이기
+            // 3-0. 리스트가 비어있는 경우
+                // 3-0-0. 현재 노드 기록
+                // 3-0-1. 노드 생성
+                // 3-0-2. 현재 노드와 생성된 노드 연결
+                // 3-0-3. 현재 노드 다음으로 이동
+                // 3-0-4. 임시 사이즈 업
+        if (size == 0) {
+            while (it.hasNext()) {
+                Node newNode = new Node(it.next());
+                curr.next = newNode;
+                newNode.prev = curr;
+                curr = curr.next;
+                tmpSize++;
+            }
+        }
+            // 3-1. 맨 앞에부터 이어 붙이는 경우
+                // 3-1-0. index 번째 노드로 이동, 현재 노드 기록
+                // 3-1-1. 노드 생성
+                // 3-1-2. first에 생성된 노드 기록
+                // 3-1-3. 생성된 노드와 현재 노드 연결
+                // 3-1-5. 임시 사이즈 업
+        else if (index == 0) {
+            if (it.hasNext()) {
+                Node newNode = new Node(it.next());
+                newNode.next = curr;
+                curr.prev = newNode;
+                first = newNode;
+                curr = newNode;
+                tmpSize++;
+
+            }
+            while (it.hasNext()) {
+                Node newNode = new Node(it.next());
+                Node next = curr.next;
+
+                newNode.prev = curr;
+                curr.next = newNode;
+
+                next.prev = newNode;
+                newNode.next = next;
+
+                curr = curr.next;
+                tmpSize++;
+            }
+        }
+        // 3-2. 중간서부터 이어 붙이는 경우
+                // 3-2-0. index 번째 노드로 이동, 현재 노드 기록
+                // 3-2-1. 노드 생성
+                // 3-2-2. 현재 노드와 생성된 노드 연결
+                // 3-2-3. 생성된 노드와 다음 노드 연결
+                // 3-2-4. 현재 노드 다음으로 이동
+                // 3-2-5. 임시 사이즈 업
+        else if (0 < index && index < size-1) {
+            while (it.hasNext()) {
+                Node newNode = new Node(it.next());
+                Node prev = curr.prev;
+
+                newNode.prev = prev;
+                prev.next = newNode;
+
+                newNode.next = curr;
+                curr.prev = newNode;
+
+                tmpSize++;
+            }
+        }
+        // 3-3. 맨 뒤에서부터 이어 붙이는 경우
+                // 3-3-0. 마지막 노드로 이동, 현재 노드 기록
+                // 3-3-1. 노드 생성
+                // 3-3-2. 현재 노드와 생성된 노드 연결
+                // 3-3-3. 현재노드 다음으로 이동
+                // 3-3-4. last 업데이트
+                // 3-3-5. 임시 사이즈 업
+        else {
+            while (it.hasNext()) {
+                Node newNode = new Node(it.next());
+
+                newNode.prev = curr;
+                curr.next = newNode;
+
+                curr = curr.next;
+
+                last = curr;
+
+                tmpSize++;
+            }
+        }
+
+        // 4. 사이즈업
+        size += tmpSize;
+    }
+
+    public Object getFirst() {
+        return first != null ? first.obj : null;
+    }
+
+    public Object getLast() {
+        return last != null ? last.obj : null;
+    }
+
+    // 뒤에서 부터 탐색해서 obj 있으면 반환
+    public int lastIndexOf(Object obj) {
+        Node curr = last;
+        int index = size-1;
+
+        while (curr != null) {
+            if (curr.obj.equals(obj)) {
+                return index;
+            }
+            curr = curr.prev;
+            index--;
+        }
+
+        return -1;
+    }
+
+    public boolean remove(Object obj) {
+        int index = indexOf(obj);
+        if (index == -1) return false;
+        Node curr = find(index);
+
+        if (index == 0) {
+            if (curr.next != null) {
+                Node next = curr.next;
+
+                curr.next = null;
+                next.prev = null;
+
+                first = next;
+            } else {
+                first = null;
+                last = null;
+            }
+        } else if (0 < index && index <=size-1) {
+            Node prev = curr.prev;
+            Node next = curr.next;
+
+            prev.next = next;
+            curr.prev = null;
+
+            next.prev = prev;
+            curr.next = null;
+        } else {
+            Node prev = curr.prev;
+
+            prev.next = null;
+            curr.prev = null;
+
+            last = prev;
+        }
+
+        size--;
+        return true;
+    }
+
 
     // 리스트 비어있는지 확인
      public boolean isEmpty() {
@@ -175,10 +409,12 @@ public class MyDoublyLinkedList {
      public int size() {
         return size;
      }
-
      // 인덱스에 객체가 존재하는지 확인
      private boolean isElementsOnIndex(int index) {
         return 0 <= index && index < size;
      }
 
+     private boolean isIncludeIndex(int index) {
+        return (0 <= index && index <= size);
+     }
 }
--- src/test/MyDoublyLinkedListTest.java
+++ src/test/MyDoublyLinkedListTest.java
@@ -5,10 +5,17 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import implemented.MyDoublyLinkedList;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.LinkedList;
+import java.util.NoSuchElementException;
+import jdk.jfr.Experimental;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 /**
  * 1차 구현 과정 - 24/01/25
@@ -21,13 +28,13 @@ import org.junit.jupiter.api.Test;
  * - 6. 존재 여부 : boolean isEmpty() : O
  *
  * 2차 구현 과정 - 24/01/26
- * - 0. 생성자 LinkedList(Collection c) :
- * - 1. 추가 : void add(int index, Object obj) :
- * - 2.     : void addAll(int index, Collection c) :
- * - 3. 조회 : int lastIndexOf(Object obj) :
- * - 4.     : Object getFirst() :
- * - 5.     : Object getLast() :
- * - 6. 삭제 : boolean remove(Object obj) :
+ * - 0. 생성자 LinkedList(Collection c) : O
+ * - 1. 추가 : void add(int index, Object obj) : O
+ * - 2.     : void addAll(int index, Collection c) : O
+ * - 3. 조회 : int lastIndexOf(Object obj) : O
+ * - 4.     : Object getFirst() : O
+ * - 5.     : Object getLast() : O
+ * - 6. 삭제 : boolean remove(Object obj) : O
  *
  */
 public class MyDoublyLinkedListTest {
@@ -49,11 +56,46 @@ public class MyDoublyLinkedListTest {
         assertTrue(mdl != null);
     }
 
-    @Test
-    @DisplayName("생성자 - 1. Collection 을 매개변수로 받는 생성자 호출")
-    void constructorTest1() {
+    @DisplayName("Collection 을 매개변수로 받는 생성자 호출")
+    @ParameterizedTest(name="{index}. {displayName} (Collection의 size는 {0})")
+    @ValueSource(ints = {0, 10, 100, 1_000, 10_000})
+    void constructorTest1(int size) {
+        // 0. 세팅값
+        ArrayList list = new ArrayList();
+        for (int i=0; i<size; i++) {
+            list.add(i);
+        }
+        Collection c = list;
 
+        // 1. 작업 실행 및 결과 기록
+        LinkedList dl = new LinkedList(c);
+        MyDoublyLinkedList mdl = new MyDoublyLinkedList(c);
+
+        // 2. 결과 비교
+        assertTrue(dl != null);
+        assertTrue(mdl != null);
+
+        assertTrue(size == dl.size());
+        assertTrue(size == mdl.size());
     }
+
+    @Test
+    @DisplayName("Collection 을 매개변수로 받는 생성자 호출, 이때 Collection이 null임")
+    void constructorTest2() {
+        // 0. 세팅값
+        Collection c = null;
+
+        try {
+            // 1. 작업 실행 및 결과 기록
+            LinkedList dl = new LinkedList(c);
+            MyDoublyLinkedList mdl = new MyDoublyLinkedList(c);
+        } catch (Exception e) {
+            // 2. 결과 비교
+            assertEquals(NullPointerException.class, e.getClass());
+        }
+        
+    }
+
 
     @Test
     @DisplayName("boolean add(Object obj) - 0. null 을 추가하려는 경우")
@@ -1078,8 +1120,220 @@ public class MyDoublyLinkedListTest {
         assertEquals(expectedObj, result2);
         assertTrue(expectedSize == mdl.size());
     }
+    
+    @DisplayName("void add(int index, Object obj) - 0. 범위를 벗어난 인덱스를 입력하는 경우")
+    @ParameterizedTest(name = "{index}. {displayName} (현재 리스트 사이즈는 10, 이상한 인덱스 : {0})")
+    @ValueSource(ints = {-1, 50000, -20, -5, 1000000})
+    void addWithNotIncludeIndexTest(int index) {
+        // 0. 세팅
+            // 사이즈 10으로 만들기
+        for (int i=0; i<10; i++) {
+            dl.add(i);
+            mdl.add(i);
+        }
+        Object obj = "obj";
+
+        // 1. 실행
+        // 2. 예측 결과
+        try {
+            dl.add(index, obj);
+            mdl.add(index, obj);
+        } catch (Exception e) {
+            assertEquals(IndexOutOfBoundsException.class, e.getClass());
+        }
+    }
+
+    @DisplayName("void add(int index, Object obj) - 1. 정상적인 실행")
+    @ParameterizedTest(name = "{index}. {displayName} (현재 리스트 사이즈는 {0})")
+    @ValueSource(ints = {1, 10, 100, 1_000, 10_000})
+    void setUpSize(int size) {
+        // 0. 세팅
+        for (int i=0; i<size; i++) {
+            dl.add(i);
+            mdl.add(i);
+        }
+        // 1. 랜덤 인덱스에 객체 추가
+        Object obj = "obj";
+        int randomIndex = (int)(Math.random() * size);
+        dl.add(randomIndex, obj);
+        mdl.add(randomIndex, obj);
+        // 2. 결과 비교
+        addTest(dl, mdl, randomIndex);
+
+    }
+
+    void addTest(LinkedList dl, MyDoublyLinkedList mdl, int index) {
+        Object o1 = dl.get(index);
+        Object o2 = mdl.get(index);
+        assertEquals(o1, o2);
+    }
+
+    @DisplayName("void addAll(int index, Collection c) - 0. 범위를 벗어난 인덱스를 입력하는 경우")
+    @ParameterizedTest(name = "{index}. {displayName} (현재 리스트 사이즈는 10, 이상한 인덱스 : {0})")
+    @ValueSource(ints = {-1, 50000, -20, -5, 1000000})
+    void addWithNotIncludeIndexTest2(int index) {
+        // 0. 세팅
+        // 사이즈 10으로 만들기
+        Collection c = new ArrayList();
+        for (int i=0; i<10; i++) {
+            dl.add(i);
+            mdl.add(i);
+            c.add(i);
+        }
+
+        // 1. 실행
+        // 2. 예측 결과
+        try {
+            dl.addAll(index, c);
+            mdl.addAll(index, c);
+        } catch (Exception e) {
+            assertEquals(IndexOutOfBoundsException.class, e.getClass());
+        }
+    }
+
+    @DisplayName("void addAll(int index, Collection c) - 1. 정상적인 실행")
+    @ParameterizedTest(name = "{index}. {displayName} (현재 리스트와 컬렉션의 사이즈는 {0})")
+    @ValueSource(ints = {1, 10, 100, 1_000, 10_000})
+    void setUpSize2(int size) {
+        // 0. 세팅
+        Collection c = new ArrayList();
+        for (int i=0; i<size; i++) {
+            dl.add(i);
+            mdl.add(i);
+            c.add(i);
+        }
+        // 1. 랜덤 인덱스에 컬렉션 추가
+        int randomIndex = (int)(Math.random() * size);
+        dl.addAll(randomIndex, c);
+        mdl.addAll(randomIndex, c);
+        // 2. 결과 비교
+        addTest2(dl, mdl);
+
+    }
 
 
+    void addTest2(LinkedList dl, MyDoublyLinkedList mdl) {
+        // 0. 사이즈 맞는지 확인
+        assertTrue(dl.size() == mdl.size());
 
+        // 1. 모든 요소 같은 객체인지 확인
+        for (int i=0; i<dl.size(); i++) {
+            Object o1 = dl.get(i);
+            Object o2 = mdl.get(i);
+            assertEquals(o1, o2);
+        }
+    }
 
+    @DisplayName("Object getFirst(), Object getLast() - 0. 리스트가 비어있는 경우")
+    @RepeatedTest(value = 10, name="{displayName} : {currentRepetition} / {totalRepetition}")
+    void getFirstAndGetLastTest0() {
+        try {
+            Object f1 = dl.getFirst();
+            Object f2 = mdl.getFirst();
+
+            Object l1 = dl.getFirst();
+            Object l2 = mdl.getLast();
+
+        } catch (Exception e) {
+            assertEquals(NoSuchElementException.class, e.getClass());
+        }
+    }
+
+    @DisplayName("Object getFirst(), Object getLast() - 1. 리스트가 차있는 경우")
+    @ParameterizedTest(name = "{index}. {displayName} (현재 리스트와 컬렉션의 사이즈는 {0})")
+    @ValueSource(ints = {1, 10, 100, 1_000, 10_000})
+    void getFirstAndGetLastTest1(int size) {
+        // 0. 세팅
+        for (int i=0; i<size; i++) {
+            dl.add(i);
+            mdl.add(i);
+        }
+
+        // 1. 비교
+        Object f1 = dl.getFirst();
+        Object f2 = mdl.getFirst();
+
+        Object l1 = dl.getLast();
+        Object l2 = mdl.getLast();
+
+        assertEquals(f1, f2);
+        assertEquals(l1, l2);
+    }
+
+    @DisplayName("int lastIndexOf(Object obj) - 0. 존재하지않는 객체를 찾는 경우")
+    @ParameterizedTest(name = "{index}. {displayName} (현재 리스트 사이즈는 {0})")
+    @ValueSource(ints = {1, 10, 100, 1_000, 10_000})
+    void lastIndexOfTest0(int size) {
+        for (int i=0; i<size; i++) {
+            dl.add(i);
+            mdl.add(i);
+        }
+
+        Object obj = "obj";
+        int result1 = dl.lastIndexOf(obj);
+        int result2 = mdl.lastIndexOf(obj);
+        assertTrue(result1 == result2);
+    }
+
+    @DisplayName("Object getFirst(), Object getLast() - 1. 리스트가 차있는 경우")
+    @ParameterizedTest(name = "{index}. {displayName} (현재 리스트와 컬렉션의 사이즈는 {0})")
+    @ValueSource(ints = {1, 10, 100, 1_000, 10_000})
+    void lastIndexOfTest1(int size) {
+        // 0. 세팅
+        int randomIndex = (int)Math.random()*size;
+        Object obj = "obj";
+        for (int i=0; i<size; i++) {
+            if (i == randomIndex) {
+                dl.add(obj);
+                mdl.add(obj);
+            } else {
+                dl.add(i);
+                mdl.add(i);
+            }
+        }
+
+        // 1. 비교
+        int result1 = dl.lastIndexOf(obj);
+        int result2 = mdl.lastIndexOf(obj);
+        assertTrue(result1 == result2);
+    }
+
+    @DisplayName("boolean remove(Object obj) - 0. 존재하는 객체를 삭제하는 경우")
+    @ParameterizedTest(name = "{index}. {displayName} (현재 리스트 사이즈는 {0})")
+    @ValueSource(ints = {1, 10, 100, 1_000, 10_000})
+    void removeTest0(int size) {
+        int randomIndex = (int)Math.random()*size;
+        Object obj = "obj";
+
+        for (int i=0; i<size; i++) {
+            if (i == randomIndex) {
+                dl.add(obj);
+                mdl.add(obj);
+            } else {
+                dl.add(i);
+                mdl.add(i);
+            }
+        }
+
+        boolean result1 = dl.remove(obj);
+        boolean result2 = mdl.remove(obj);
+        assertTrue(result1 == result2);
+    }
+
+    @DisplayName("boolean remove(Object obj) - 1. 존재하는 객체를 삭제하는 경우")
+    @ParameterizedTest(name = "{index}. {displayName} (현재 리스트 사이즈는 {0})")
+    @ValueSource(ints = {1, 10, 100, 1_000, 10_000})
+    void removeTest1(int size) {
+        Object obj = "obj";
+
+        for (int i=0; i<size; i++) {
+            dl.add(i);
+            mdl.add(i);
+
+        }
+
+        boolean result1 = dl.remove(obj);
+        boolean result2 = mdl.remove(obj);
+        assertTrue(result1 == result2);
+    }
 }
